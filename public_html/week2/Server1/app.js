ec453191591708d9b42ef30d21025e52c131ed29
RyCab <rcabral0805@gmail.com>
2022-01-16 15:48:33 -0500

    commit

---
+++ public_html/week2/Server1/app.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var port = 3000
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+"/views/index.html"))
+})
+
+app.use(express.static(__dirname+"/views"))
+
+app.listen(port, function(){
+    console.log("Connected at port: " + port);
+})
+
+app.get('/', function(request, response){
+    response.writeHead(301, {'Location': "http://" + request.headers['host'] + '/index.html' });
+})
+
+    
