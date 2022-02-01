d0a21d55ca41fb895ee8913a95c3fdc3e6672bb5
008003309 <008003309@N210C5>
2022-01-31 20:08:39 -0500

    class

---
+++ TemplateExample/app.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const router = require('./app_server/routes/index')
+const hbs = require('handlebars')
+
+app.use('/', router)
+
+//Set the view engine
+app.engine('hbs', exphbs.engine({
+    defaultLayout:'main',
+    extname:'.hbs',
+    helpers:{
+        getShortComment(comment){
+            if(comment.length < 60){
+                return comment
+            }
+            return comment.substring(0,60)+'...'
+        },
+
+        formatPhoneNumber(phonenumber){
+            if(phonenumber){
+                let phone = phonenumber.toString()
+                return "(" + phone.substring(0,3) + ") " + phone.substring(3,6) + "-" + phone.substring(6,10)
+            }
+        },
+
+        makeBold(address){
+            return new hbs.SafeString("<strong>" + address + "</strong>")
+        },
+
+        formatEmail(email){
+            return new hbs.SafeString(`<a href="mailto:${email}">${email}</a>`)
+        },
+
+        upperCaseWord(word){
+            if(word){
+                return word.toUpperCase()
+            }
+        }
+
+
+    }
+}))
+
+app.set('view engine', 'hbs')
+
+//route to render the page
+app.get('/',(req,res)=>{
+    res.render('home', {
+        post:[
+            {
+                author:"Schooly D",
+                image:"https://picsum.photos/500/500",
+                comments:['comment 1', 'comment 2', 'Eiusmod occaecat nostrud excepteur anim velit reprehenderit officia sunt nostrud incididunt. Ea commodo qui deserunt ea pariatur mollit non quis Lorem sint est qui labore aliquip. Eiusmod excepteur elit veniam quis quis officia.']
+            },
+            {
+                author:"Jordan D",
+                image:"https://picsum.photos/500/500?2",
+                comments:[]
+            },
+            {
+                author:"Terry B",
+                image:"https://picsum.photos/500/500?3",
+                comments:['yo this is awesome', 'Proident qui tempor aute Lorem culpa Lorem do labore cillum veniam veniam veniam commodo cupidatat. Et nisi anim veniam eiusmod fugiat labore ad minim. Quis anim esse fugiat ullamco non.', 'this is the best']
+            },
+        ]
+    })
+})
+
+//set up port for connection
+app.listen(3000, ()=>{
+    console.log("Connected on port 3000")
+})
---
+++ TemplateExample/app_server/controllers/pages.js
@@ -0,0 +1,41 @@
+const mainPage = (req, res)=>{
+    res.render('home', {
+        post:[
+            {
+                author:"Schooly D",
+                image:"https://picsum.photos/500/500",
+                comments:['comment 1', 'comment 2', 'Eiusmod occaecat nostrud excepteur anim velit reprehenderit officia sunt nostrud incididunt. Ea commodo qui deserunt ea pariatur mollit non quis Lorem sint est qui labore aliquip. Eiusmod excepteur elit veniam quis quis officia.']
+            },
+            {
+                author:"Jordan D",
+                image:"https://picsum.photos/500/500?2",
+                comments:[]
+            },
+            {
+                author:"Terry B",
+                image:"https://picsum.photos/500/500?3",
+                comments:['yo this is awesome', 'Proident qui tempor aute Lorem culpa Lorem do labore cillum veniam veniam veniam commodo cupidatat. Et nisi anim veniam eiusmod fugiat labore ad minim. Quis anim esse fugiat ullamco non.', 'this is the best']
+            },
+        ]
+    })
+}
+
+const pageTwo = (req, res)=>{
+    res.render('index', {
+        title:"Page Two",
+        name:"Ry",
+        phonenumber:"4015556555",
+        address:"1 New England Tech BLVD",
+        email:"Ryan@gmail.com",
+        occupation:"NFTs"
+    })
+}
+const pageThree = (req, res)=>{
+    res.render('about', {title:"Page Three"})
+}
+
+module.exports = {
+    mainPage,
+    pageTwo,
+    pageThree
+}
